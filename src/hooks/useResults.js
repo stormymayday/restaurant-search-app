bf46f92a1eb900f50f9352f7f511b6c9873916f5
stormymayday <stormymayday7@gmail.com>
2024-05-09 11:56:54 -0700

    Switched to thecocktaildb api instead of yelp

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -1,30 +1,54 @@
 import { useEffect, useState } from "react";
-import yelp from "../api/yelp";
+// import yelp from "../api/yelp";
+import cocktaildb from "../api/cocktaildb";
 
 export default () => {
     const [results, setResults] = useState([]);
     const [errorMessage, setErrorMessage] = useState("");
 
     const searchApi = async (search) => {
+        // YELP - START
+        // try {
+        //     const response = await yelp.get("/search", {
+        //         params: {
+        //             limit: 50,
+        //             term: search,
+        //             location: "san jose",
+        //         },
+        //     });
+        //     setResults(response.data.businesses);
+        //     setErrorMessage("");
+        // } catch (err) {
+        //     console.log(err);
+        //     setErrorMessage("Something went wrong");
+        // }
+        // YELP - END
+
+        // COCKTAIL DB - START
         try {
-            const response = await yelp.get("/search", {
+            const response = await cocktaildb.get("/search.php", {
                 params: {
-                    limit: 50,
-                    term: search,
-                    location: "san jose",
+                    s: search,
                 },
             });
-            setResults(response.data.businesses);
-            setErrorMessage("");
+
+            if (response.data.drinks) {
+                setResults(response.data.drinks);
+                setErrorMessage("");
+            } else {
+                setResults([]);
+                setErrorMessage("No results");
+            }
         } catch (err) {
             console.log(err);
             setErrorMessage("Something went wrong");
         }
+        // COCKTAIL DB - END
     };
 
     // Calling searchApi once when component is first rendered
     useEffect(() => {
-        searchApi("pasta");
+        searchApi("a");
     }, []);
 
     return [searchApi, results, errorMessage];
