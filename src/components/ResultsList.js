7a33ccec8e1226c3f0b16be58897cf7d63a8498a
stormymayday <stormymayday7@gmail.com>
2024-05-14 19:39:50 -0700

    Passing down the navigation object directly into ResultsList via withNavigation function

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -6,6 +6,7 @@ import {
     TouchableOpacity,
 } from "react-native";
 import ResultsDetail from "./ResultsDetail";
+import { withNavigation } from "react-navigation";
 
 const ResultsList = ({ title, results, navigation }) => {
     return (
@@ -50,4 +51,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
