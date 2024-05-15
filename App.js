0bb48eb137c5c701b42e1560c3976bafd3534122
stormymayday <stormymayday7@gmail.com>
2024-05-14 19:25:56 -0700

    Wired ResultsShowScreen to the StackNavigator

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
 import SearchScreen from "./src/screens/SearchScreen";
+import ResultsShowScreen from "./src/screens/ResultsShowScreen";
 
 // Creating a Navigator object
 const navigator = createStackNavigator(
     {
         Search: SearchScreen,
+        ResultsShow: ResultsShowScreen,
     },
     {
         initialRouteName: "Search",
