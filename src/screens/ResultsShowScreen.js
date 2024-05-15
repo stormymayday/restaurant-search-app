afd9a8b31be694c61bf9c5211cccc81aa442d8f9
stormymayday <stormymayday7@gmail.com>
2024-05-14 19:23:11 -0700

    Created ResultsShowScreen component

---
+++ src/screens/ResultsShowScreen.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+
+const ResultsShowScreen = (props) => {
+    return (
+        <View>
+            <Text>Results Show Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsShowScreen;
