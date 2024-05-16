43a2266feac56c921f6b1733dfce8896e256317a
stormymayday <stormymayday7@gmail.com>
2024-05-15 17:42:53 -0700

    Fetching a single result

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,8 +1,26 @@
 import { View, Text, StyleSheet } from "react-native";
+import cocktaildb from "../api/cocktaildb";
+import { useEffect, useState } from "react";
 
 const ResultsShowScreen = (props) => {
     const { navigation } = props;
     const id = navigation.getParam("id");
+    const [result, setResult] = useState(null);
+
+    const getResult = async (id) => {
+        try {
+            const response = await cocktaildb.get(`/lookup.php?i=${id}`);
+            // console.log(`Response ${JSON.stringify(response)}`);
+            setResult(response.data);
+        } catch (error) {
+            console.error(`Oops there was an error :${error}`);
+        }
+    };
+
+    useEffect(() => {
+        getResult(id);
+    }, []);
+
     return (
         <View>
             <Text>Results Show Screen</Text>
